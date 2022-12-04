d280b1b2f52143c9528881679b0aa9273f6fa669
Andrea Kis-Lodar <lodarandrea@gmail.com>
2022-12-04 17:22:11 +0100

    dashboard layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,31 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg-gray-300'>
+      <div className=' bg-white flex flex-col text-gray-900 rounded-xl h-screen'>
+        <header className='flex justify-between mt-3 mx-3'>
+          <img src={logo} className="h-10" alt="logo" />
+          <a className='pr-5' href='#' > ... </a>
+        </header>
+        <div className='flex flex-col flex-1 mt-10'>
+          <div className='contentItems'>
+            <a href='#'>Customers</a>
+          </div>
+          <div className='contentItems'>
+            <a href='#'>Instructors</a>
+          </div>
+          <div className='contentItems'>
+            <a href='#'>Profile</a>
+          </div>
+        </div>
+        <div className='flex justify-center font-semibold mb-8'>
+          <a href='#' className='px-5'>Customers</a>
+          <a href='#' className='px-5'>Instructors</a>
+          <a href='#' className='px-5'>Profile</a>
+        </div>
+      </div>
     </div>
-  );
+    );
 }
 
 export default App;
