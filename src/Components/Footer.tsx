2c61af1c9b328fd11cd7a3f302fc94a0fd9ea9b4
Andrea Kis-Lodar <lodarandrea@gmail.com>
2022-12-15 11:39:43 +0100

    updating Footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,16 +1,30 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {
+  HomeIcon,
+  UserGroupIcon,
+  AcademicCapIcon,
+} from '@heroicons/react/24/outline'
 
 function Footer() {
   return (
-    <div className="flex justify-center font-semibold mb-8">
-      <Link to="/" className="px-5">
+    <div className="flex justify-center mb-8 font-semibold text-xs text-center">
+      <Link to="/" className="px-5 active:translate-y-1 transform transition">
+        <HomeIcon className="w-12 h-12 p-2 m-auto" />
         Home
       </Link>
-      <Link to="/customers" className="px-5">
+      <Link
+        to="/customers"
+        className="px-5 active:translate-y-1 transform transition"
+      >
+        <UserGroupIcon className="w-12 h-12 p-2 m-auto" />
         Customers
       </Link>
-      <Link to="/instructors" className="px-5">
+      <Link
+        to="/instructors"
+        className="px-5 active:translate-y-1 transform transition"
+      >
+        <AcademicCapIcon className="w-12 h-12 p-2 m-auto" />
         Instructors
       </Link>
     </div>
