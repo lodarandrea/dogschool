fc56da19b66a38f78cf53fa1ed88fdce2e505f36
Andrea Kis-Lodar <lodarandrea@gmail.com>
2023-01-03 10:30:02 +0100

    saving changes

--- src/Components/Buttons/Logout.tsx
+++ src/Components/Buttons/Logout.tsx
@@ -5,7 +5,10 @@ const LogoutButton = () => {
   const { logout } = useAuth0()
 
   return (
-    <button onClick={() => logout({ returnTo: window.location.origin })}>
+    <button
+      onClick={() => logout({ returnTo: window.location.origin })}
+      className="block m-auto text-lg font-semibold rounded-lg border-2 bg-slate-500 text-white active:translate-y-1 transform transition px-5 py-3"
+    >
       Log Out
     </button>
   )
--- src/pages/CustomerMe.tsx
+++ src/pages/CustomerMe.tsx
@@ -1,17 +1,24 @@
-import ExitButton from '../Components/Buttons/ExitButton'
+import { useAuth0 } from '@auth0/auth0-react'
+import LogoutButton from '../Components/Buttons/Logout'
 import QRCodeButton from '../Components/Buttons/QRCodeButton'
 import dog from '../img/dog.png'
 
 function CustomerMe() {
-  return (
+  const { user, isAuthenticated, isLoading } = useAuth0()
+
+  if (isLoading) {
+    return <div>Loading ...</div>
+  }
+
+  return isAuthenticated ? (
     <div className="mx-8 my-3">
       <img className="w-56 bg-gray-300 p-10 m-auto" src={dog} alt="customer" />
       <h1 className="p-8 text-xl text-center">Name of the Customer</h1>
       <h2 className="p-6 text-lg text-center">Description and details</h2>
       <QRCodeButton />
-      <ExitButton />
+      <LogoutButton />
     </div>
-  )
+  ) : null
 }
 
 export default CustomerMe
