34631a9676303d2cf5d5d5cfe778b53142624b49
Andrea Kis-Lodar <lodarandrea@gmail.com>
2023-01-17 09:04:32 +0100

    saving changes

---
+++ src/Components/Buttons/ReadQRCodeButton.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import { useZxing } from 'react-zxing'
+
+function ReadQRCodeButton() {
+  const [message, setMessage] = useState({})
+  const [camera, setCamera] = useState(false)
+  const { ref } = useZxing({
+    paused: camera,
+    onResult(result) {
+      console.log(result)
+      fetch(`${process.env.REACT_APP_API_URL}/customers/{id}/attend`, {
+        method: 'PUT',
+      })
+        .then((response) => response.json())
+        .then((data) => setMessage(data))
+    },
+  })
+
+  return (
+    <div>
+      <button onClick={() => setCamera((c) => !c)}>Read QR Code</button>
+      <video ref={ref} />
+    </div>
+  )
+}
+
+export default ReadQRCodeButton
