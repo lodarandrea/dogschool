3df881b4fff2146286536364f73ba942d321f2cd
Andrea Kis-Lodar <lodarandrea@gmail.com>
2023-01-19 13:15:49 +0100

    implementing the QR Code reader

--- src/Components/Buttons/ReadQRCodeButton.tsx
+++ src/Components/Buttons/ReadQRCodeButton.tsx
@@ -1,39 +1,15 @@
-import { useEffect, useState } from 'react'
-import { useZxing } from 'react-zxing'
+import { QrCodeIcon } from '@heroicons/react/24/outline'
 
-function ReadQRCodeButton() {
-  const [message, setMessage] = useState(null)
-  const [camera, setCamera] = useState(true)
-  const [qrResult, setQrResult]: any = useState(null)
-  const { ref } = useZxing({
-    paused: camera,
-    onResult(result) {
-      console.log(result)
-      setQrResult(result.getText())
-    },
-  })
-  useEffect(() => {
-    console.log(qrResult)
-    if (qrResult) {
-      console.log(qrResult)
-      fetch(`${process.env.REACT_APP_API_URL}/customers/1/attend`, {
-        method: 'PUT',
-      })
-        .then((response) => response.json())
-        .then((data) => setMessage(data.message))
-      console.log(qrResult)
-    }
-  }, [qrResult])
-  console.log(message)
-  function handleClick() {
-    setCamera((c) => !c)
-  }
+function ReadQRCodeButton(props: {
+  setCamera: (value: (c: boolean) => boolean) => void
+}) {
   return (
-    <>
-      <button onClick={handleClick}>Read QR Code</button>
-      <video ref={ref} />
-      {message ? window.alert(message) : null}
-    </>
+    <div className=" text-xs font-semibold p-5 text-center active:translate-y-1 transform transition;">
+      <button onClick={() => props.setCamera((c) => !c)}>
+        <QrCodeIcon className="w-9 m-auto" />
+        Read QR Code
+      </button>
+    </div>
   )
 }
 
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,8 +1,42 @@
+import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { useZxing } from 'react-zxing'
 import '../App.css'
 import ReadQRCodeButton from '../Components/Buttons/ReadQRCodeButton'
 
 function Dashboard() {
+  const [message, setMessage] = useState('')
+  const [camera, setCamera] = useState(false)
+  const [qrResult, setQrResult]: any = useState(null)
+  const { ref } = useZxing({
+    paused: !camera,
+    onResult(result) {
+      console.log(result)
+      setQrResult(result.getText())
+      setCamera(false)
+    },
+  })
+  useEffect(() => {
+    if (qrResult) {
+      toast.promise(
+        fetch(`${process.env.REACT_APP_API_URL}/customers/1/attend`, {
+          method: 'PUT',
+        })
+          .then((response) => response.json())
+          .then((data) => setMessage(data.message)),
+        {
+          success: message,
+          error: 'Promise rejected 🤯',
+        },
+        {
+          position: toast.POSITION.TOP_CENTER,
+        }
+      )
+    }
+  }, [message, qrResult])
+
   return (
     <div>
       <div className="contentItems">
@@ -14,8 +48,15 @@ function Dashboard() {
       <div className="contentItems">
         <Link to={`instructors/1`}> Profile</Link>
       </div>
-      <div className="contentItems">
-        <ReadQRCodeButton />
+      <div className="flex justify-center items-end">
+        <ReadQRCodeButton setCamera={setCamera} />
+      </div>
+      <div>
+        {camera ? (
+          <div className="flex justify-center items-center fixed inset-0 bg-slate-700/80 ">
+            <video ref={ref} className="max-w-md" />
+          </div>
+        ) : null}
       </div>
     </div>
   )
