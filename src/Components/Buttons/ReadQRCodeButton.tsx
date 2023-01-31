88b076452e74bdcc523e9779cf87e6ef7be3ba99
Andrea Kis-Lodar <lodarandrea@gmail.com>
2023-01-31 21:38:01 +0100

    updating the Dashboard

--- src/Components/Buttons/ReadQRCodeButton.tsx
+++ src/Components/Buttons/ReadQRCodeButton.tsx
@@ -4,9 +4,13 @@ function ReadQRCodeButton(props: {
   setCamera: (value: (camera: boolean) => boolean) => void
 }) {
   return (
-    <div className=" text-xs font-semibold p-5 text-center active:translate-y-1 transform transition;">
-      <button onClick={() => props.setCamera((camera) => !camera)}>
-        <QrCodeIcon className="w-9 m-auto" />
+    <div className="text-xs font-bold p-5 text-center active:translate-y-1 transform transition;">
+      <button
+        onClick={() => props.setCamera((camera) => !camera)}
+        className="p-2 bg-turquoise-200 text-turquoise-900 shadow-inner shadow-neutral-200 rounded-xl"
+        // className=" text-turquoise-900"
+      >
+        <QrCodeIcon className="w-9 inline" />
         Read QR Code
       </button>
     </div>
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,12 +2,13 @@ import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import ReadQRCodeButton from '../Components/Buttons/ReadQRCodeButton'
 import QRReader from '../Components/QRReader'
+import bgPaw from '../img/bgPaw.png'
 
 function Dashboard() {
   const [camera, setCamera] = useState(false)
 
   return (
-    <div>
+    <div style={{ backgroundImage: `url(${bgPaw})`, backgroundSize: 'cover' }}>
       <div className="contentItems">
         <Link to={'/customers'}>Customers</Link>
       </div>
@@ -17,7 +18,7 @@ function Dashboard() {
       <div className="contentItems">
         <Link to={`instructors/1`}> Profile</Link>
       </div>
-      <div className="flex justify-center items-end">
+      <div className=" justify-center items-end">
         <ReadQRCodeButton setCamera={setCamera} />
       </div>
       <div>{camera ? <QRReader setCamera={setCamera} /> : null}</div>
